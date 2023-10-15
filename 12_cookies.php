40cc547b90dbd7a427233ab2bd183af9b625ea14
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-10-15 14:49:57 +0100

    set and unset cookies in php

--- 12_cookies.php
+++ 12_cookies.php
@@ -4,3 +4,13 @@
 /*
   Cookies are a mechanism for storing data in the remote browser and thus tracking or identifying return users. You can set specific data to be stored in the browser, and then retrieve it when the user visits the site again.
 */
+
+// SET COOKIES
+setcookie('name', 'Emma', time() + 86400 * 30);
+
+if(isset($_COOKIE['name'])){
+  echo 'Hello ' . $_COOKIE['name'];
+};
+
+// UNSET COOKIES
+setcookie('name', '', time() - 86400);
