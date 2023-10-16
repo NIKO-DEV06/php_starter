c5cbfc5450d180c2458a4224eef02264cfd9ca9e
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-10-16 23:34:34 +0100

    connected php app to mysql db

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -1,5 +1,28 @@
 <?php include 'inc/header.php'; ?>
-    <h2>Feedback</h2>
+
+<?php
+$feedback = [
+  [
+    'id' => '1',
+    'name' => 'Beth Williams',
+    'email' => 'beth@gmail.com',
+    'body' => 'Traversy Media is ok'
+  ],
+  [
+    'id' => '2',
+    'name' => 'Walt Williams',
+    'email' => 'walt@gmail.com',
+    'body' => 'Traversy Media sucks'
+  ],
+  [
+    'id' => '3',
+    'name' => 'Bill Johnson',
+    'email' => 'bill@gmail.com',
+    'body' => 'Traversy Media rocks'
+  ]
+]
+?>
+    <h2>Past Feedback</h2>
     <div class="card my-3">
      <div class="card-body">
        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
